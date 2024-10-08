4f8234dc99d631ad5881e60c3b376cd28cc466fe
unknown <estherakinlade04@gmail.com>
2024-10-08 15:31:54 +0100

    made changes to insert data file

--- insertData.php
+++ insertData.php
@@ -2,10 +2,10 @@
 //Give the name of the program here
 //Include your name and the date here
 //Give a brief description of what the program does
-$servername = "localhost";
-$username = "user";
+$servername = "127.0.0.1";
+$username = "root";
 $password = "";
-$dbname = "tenisclub";
+$dbname = "tennisclub";
 $port = 3306;
 
 //set the default timezone - this is necessary since MySQL 8. This is an effort to store all dates and times together with their timezones. 
