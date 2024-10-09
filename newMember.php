6771373965aa2ce95081ecce1d4e4c5beaa71c21
unknown <estherakinlade04@gmail.com>
2024-10-09 11:14:22 +0100

    added newMember.php and updated Part2.php to process form data

---
+++ newMember.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New Member</title>
+</head>
+<body>
+    <h1>Add New Member</h1>
+    <form action="Part2.php" method="POST">
+        <label for="firstname">First Name:</label>
+        <input type="text" id="firstname" name="firstname" required>
+        <br>
+        <label for="surname">Surname:</label>
+        <input type="text" id="surname" name="surname" required>
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
